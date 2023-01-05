26d374d70a8d66c62eb1f3d578350731feaea8df
dangel0n <ura.vlasov.ru@gmail.com>
2023-01-05 13:07:35 +0200

    1

--- app/js/main.js
+++ app/js/main.js
@@ -25,7 +25,8 @@ $(function () {
     numStars: 1,
     readOnly: true,
     normalFill: "#FFB800",
-    starWidth: "16px"
+    starWidth: "16px",
+    starSvg: '<svg width="16" height="16" viewBox="0 0 16 16" fill="none"xmlns="http://www.w3.org/2000/svg"><path d="M0.0229731 5.79566C0.0780973 5.62595 0.224753 5.50226 0.401315 5.47663L5.36139 4.75586L7.57966 0.26128C7.6586 0.101281 7.82157 0 7.99997 0C8.17841 0 8.34135 0.101281 8.42032 0.26128L10.6387 4.75586L15.5987 5.47663C15.7752 5.50226 15.9219 5.62595 15.977 5.79563C16.0322 5.96535 15.9862 6.15163 15.8584 6.27616L12.2694 9.77471L13.1165 14.7148C13.1467 14.8907 13.0744 15.0683 12.9301 15.1733C12.8484 15.2326 12.7517 15.2628 12.6545 15.2628C12.5799 15.2628 12.505 15.245 12.4365 15.2089L8 12.8765L3.56374 15.2089C3.40577 15.2919 3.21443 15.2781 3.07009 15.1732C2.92574 15.0683 2.8534 14.8906 2.88356 14.7147L3.73096 9.77471L0.141566 6.27613C0.0138168 6.15163 -0.0322151 5.96535 0.0229731 5.79566Z"fill="#FFB800"/></svg>'
   });
 
   $('.header__btn').on('click', function () {
@@ -50,6 +51,86 @@ $(function () {
   $('.menu-mob__close').on('click', function () {
     $('.menu-mob').removeClass('menu-mob--active')
   });
+
+  $('.category-btn').on('click', function () {
+    $('.category__form').toggleClass('category__form--active')
+    $('.category-btn').toggleClass('show')
+  });
+
+  $('.offers-btn').on('click', function () {
+    $('.offers__form').toggleClass('offers__form--active')
+    $('.offers-btn').toggleClass('show')
+  });
+
+  $('.filter-price-btn').on('click', function () {
+    $('.filter-price__form').toggleClass('filter-price__form--active')
+    $('.filter-price-btn').toggleClass('show')
+  });
+
+  var $range = $('.filter-price__input');
+  var $inputFrom = $('.filter-price__from');
+  var $inputTo = $('.filter-price__to');
+  var instance;
+  var min = 0;
+  var max = 1000;
+  var from = 0;
+  var to = 0;
+
+  $range.ionRangeSlider({
+    type: "double",
+    min: min,
+    max: max,
+    from: 200,
+    to: 800,
+    onStart: updateInputs,
+    onChange: updateInputs,
+    onFinish: updateInputs
+  });
+  instance = $range.data("ionRangeSlider");
+
+  function updateInputs(data) {
+    from = data.from;
+    to = data.to;
+
+    $inputFrom.prop("value", from);
+    $inputTo.prop("value", to);
+  }
+
+  $inputFrom.on("change", function () {
+    var val = $(this).prop("value");
+
+    // validate
+    if (val < min) {
+      val = min;
+    } else if (val > to) {
+      val = to;
+    }
+
+    instance.update({
+      from: val
+    });
+
+    $(this).prop("value", val);
+
+  });
+
+  $inputTo.on("change", function () {
+    var val = $(this).prop("value");
+
+    if (val < from) {
+      val = from;
+    } else if (val > max) {
+      val = max;
+    }
+
+    instance.update({
+      to: val
+    });
+
+    $(this).prop("value", val);
+  });
+
+  $('.select-style').styler();
 
   var containerEl1 = document.querySelector('.top__content');
   var containerEl2 = document.querySelector('.stock__content');
--- gulpfile.js
+++ gulpfile.js
@@ -36,7 +36,7 @@ function styles() {
     .pipe(concat('style.min.css'))
     .pipe(autoprefixer({
       overrideBrowserslist: ['last 10 versions'],
-
+      grid: true,
     }))
     .pipe(dest('app/css'))
     .pipe(browserSync.stream())
@@ -47,7 +47,9 @@ function scripts() {
     'node_modules/jquery/dist/jquery.js',
     'node_modules/slick-carousel/slick/slick.js',
     'node_modules/mixitup/dist/mixitup.js',
+    'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
     'node_modules/rateyo/src/jquery.rateyo.js',
+    'node_modules/jquery-form-styler/dist/jquery.formstyler.js',
     'app/js/main.js'
   ])
     .pipe(concat('main.min.js'))
@@ -83,7 +85,7 @@ function svgSprites() {
       parserOptions: { xmlMode: true },
     })
     )
-    .pipe(replace('&gt;', '>')) // боремся с заменой символа 
+    .pipe(replace('&gt;', '>')) 
     .pipe(
       svgSprite({
         mode: {
